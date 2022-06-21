660b9570f9df22b4e51ea21439f4ef5ade43d67f
babhijit <abhijitb@gmail.com>
2022-06-21 22:34:36 +0530

    820. Short Encoding of Words
    Time: O(n * w) : n => number of words, w => Length of String
    Space: O(n)

--- src/main/java/com/leetcode/algo/string/short_encoding_words_820/ShortWordsEncoding.java
+++ src/main/java/com/leetcode/algo/string/short_encoding_words_820/ShortWordsEncoding.java
@@ -1,38 +1,21 @@
 package com.leetcode.algo.string.short_encoding_words_820;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Objects;
+import java.util.*;
 
 public class ShortWordsEncoding {
     public int minimumLengthEncoding(String[] words) {
-        Arrays.sort(words, Comparator.comparingInt(String::length).reversed());
+        var encodedWords = new HashSet<>(Arrays.asList(words));
+        for (var word: words) {
+            for (int i = 1; i < word.length(); ++i) {
+                encodedWords.remove(word.substring(i));
+            }
+        }
 
         int encodedStringLength = 0;
-        for (int i = 0; i < words.length; ++i) {
-            if (!wordCanBePartOfExistingEncodedString(words, i)) {
-                encodedStringLength += (words[i].length() + 1); // space for #
-            }
+        for (var word: encodedWords) {
+            encodedStringLength += word.length() + 1;
         }
 
         return encodedStringLength;
-    }
-
-    private boolean wordCanBePartOfExistingEncodedString(String[] words, int lastIndex) {
-        boolean canBePartOfExistingEncoding = false;
-
-        final String word = words[lastIndex];
-        final int wordLength = word.length();
-        for (int i = 0; i < lastIndex; ++i) {
-            String referenceWord = words[i];
-            int substringStart = referenceWord.length() - wordLength;
-            if (substringStart >= 0) {
-                if (Objects.equals(word, referenceWord.substring(substringStart))) {
-                    return true;
-                }
-            }
-        }
-
-        return canBePartOfExistingEncoding;
     }
 }
